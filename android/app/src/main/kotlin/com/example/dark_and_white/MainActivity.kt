ea5bb117f693814f88ec1dc077e1e9ff04384931
Akashellath <akashchandran367@gmail.com>
2024-06-29 10:04:35 +0530

    secon commit

---
+++ android/app/src/main/kotlin/com/example/dark_and_white/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dark_and_white
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,204 @@
+
+
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
+}
+
+class MyApp extends StatelessWidget {
+  final AdaptiveThemeMode? savedThemeMode;
+
+  const MyApp({super.key, this.savedThemeMode});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.blue,
+      ),
+      dark: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.blue,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'Adaptive Theme Demo',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const MyHomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+      debugShowFloatingThemeButton: true,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(
+            child: Padding(
+          padding: const EdgeInsets.only(top: 50, left: 10, bottom: 20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.history,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "History",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.dark_mode,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Dark Mode",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Switch(
+                    value: AdaptiveTheme.of(context).mode.isDark,
+                    onChanged: (value) {
+                      if (value) {
+                        AdaptiveTheme.of(context).setDark();
+                      } else {
+                        AdaptiveTheme.of(context).setLight();
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Weather App',
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 200, 0),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [Icon(Icons.search)],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "IN",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: 25,
+                    width: 2,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Kottakkal",
+                    style: TextStyle(fontSize: 30),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10,
+              ),
+              child: Row(
+                children: [Text("Scattered clouds")],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 55, top: 25),
+              child: Row(
+                children: [
+                  Text(
+                    "34",
+                    style: TextStyle(fontSize: 80),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Icon(
+                    Icons.cloud,
+                    size: 60,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 150,
+              width: MediaQuery.of(context).size.width / 1.08,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(255, 192, 191, 191)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [Icon(Icons.thermostat,size: 80,), Row(
+                        children: [
+                          Text("34",style: TextStyle(fontSize: 20),),Icon(Icons.heat_pump)
+
+                          // 
+                          // 
+                          // 
+                          // 
+                          // 
+
+                        ],
+                      )],
+                    ),
+                    Column(
+                      children: [Icon(Icons.water_drop,size: 80), Text("34",style: TextStyle(fontSize: 20))],
+                    ),
+                    Column(
+                      children: [Icon(Icons.air,size: 80), Text("34",style: TextStyle(fontSize: 20))],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
